58e09349ae9f61b4578f69a2cfade5daba58ee31
Gabfuwak <gabin.maury0@gmail.com>
2025-01-15 22:10:19 +0100

    fixed stupid typechecker + feat: "did you mean.."

--- typechecker.ml
+++ typechecker.ml
@@ -22,12 +22,104 @@ let rec is_subclass subcls supercls cenv =
     let subclass = Env.find subcls cenv in
     match subclass.parent with
     | None -> false
-    | Some subcls_parent -> is_subclass subcls_parent supercls cenv         
+    | Some subcls_parent -> is_subclass subcls_parent supercls cenv
 
+let levenshtein_dist word1 word2 =
+  let m = String.length word1 in
+  let n = String.length word2 in
+  
+  (* Création de la matrice *)
+  let d = Array.make_matrix (m + 1) (n + 1) 0 in
+  
+  (* Initialisation des bords *)
+  for i = 0 to m do
+    d.(i).(0) <- i
+  done;
+  for j = 0 to n do
+    d.(0).(j) <- j
+  done;
+  
+  (* Calcul des distances *)
+  for j = 1 to n do
+    for i = 1 to m do
+      let cost = if word1.[i-1] = word2.[j-1] then 0 else 1 in
+      let del = d.(i-1).(j) + 1 in         (* deletion *)
+      let ins = d.(i).(j-1) + 1 in         (* insertion *)
+      let sub = d.(i-1).(j-1) + cost in    (* substitution *)
+      d.(i).(j) <- min del (min ins sub)
+    done
+  done;
+  
+  d.(m).(n)
+
+(* Helper qui retourne tous les attributs d'une classe, y compris hérités *)
+let rec get_all_attrs cls_name cenv =
+  let cls = Env.find cls_name cenv in
+  let parent_attrs = match cls.parent with
+    | None -> []
+    | Some parent_name -> get_all_attrs parent_name cenv
+  in
+  parent_attrs @ cls.attributes
+
+(* Helper qui retourne toutes les méthodes d'une classe, y compris héritées *)
+let rec get_all_meths cls_name cenv =
+  let cls = Env.find cls_name cenv in
+  let parent_meths = match cls.parent with
+    | None -> []
+    | Some parent_name -> get_all_meths parent_name cenv
+  in
+  cls.methods @ parent_meths
+
+(* Suggère une classe *)
+let suggest_class word cenv =
+  let classes = Env.bindings cenv in
+  let distances = List.map (fun (name, _) ->
+    (name, levenshtein_dist word name)
+  ) classes in
+  match List.sort (fun (_, d1) (_, d2) -> compare d1 d2) distances with
+  | (name, dist) :: _ when dist <= 3 -> name
+  | _ -> word
+
+(* Suggère un attribut ou variable globale *)
+let suggest_attr_or_var word cls_name tenv cenv = 
+  let attrs = match cls_name with
+    | Some cls_name -> List.map fst (get_all_attrs cls_name cenv)
+    | None -> [] 
+  in
+  let vars = List.map fst (Env.bindings tenv) in
+  let all_names = attrs @ vars in
+  let distances = List.map (fun name ->
+    (name, levenshtein_dist word name)
+  ) all_names in
+  match List.sort (fun (_, d1) (_, d2) -> compare d1 d2) distances with
+  | (name, dist) :: _ when dist <= 2 -> name
+  | _ -> word
+
+(* Suggère une méthode *)
+let suggest_meth word cls_name cenv =
+  let meths = List.map 
+    (fun m -> m.method_name) 
+    (get_all_meths cls_name cenv) in
+  let distances = List.map (fun name ->
+    (name, levenshtein_dist word name)
+  ) meths in
+  match List.sort (fun (_, d1) (_, d2) -> compare d1 d2) distances with
+  | (name, dist) :: _ when dist <= 2 -> name
+  | _ -> word
 
 let typecheck_prog p =
-  let tenv = add_env p.globals Env.empty in
-  let cenv = add_classes p.classes Env.empty in
+    let cenv = add_classes p.classes Env.empty in
+  
+    (* Vérifier que les types des variables globales existent *)
+    List.iter (fun (_, typ) ->
+      match typ with
+      | TClass class_name ->
+          if not (Env.mem class_name cenv) then
+            error ("Unknown class \"" ^ class_name ^ "\". Did you mean \"" ^ suggest_class class_name cenv ^ "\"?")
+      | _ -> ()
+    ) p.globals;
+
+    let tenv = add_env p.globals Env.empty in
 
   let rec check e typ tenv cenv = 
     let typ_e = type_expr e tenv cenv in
@@ -73,43 +165,48 @@ let typecheck_prog p =
             TBool
         | _ -> failwith "Binop implementation missing (should be unreachable)";
     )
-    | New(class_name) -> 
+    | New(class_name) -> (
         try ( 
-          let _ = Env.find cenv class_name in
+          let _ = Env.find class_name cenv in
           TClass class_name
         )
-        with Not_found -> error("Class \"" ^ class_name ^ "\" does not exist." );
-    | NewCstr(class_name, args) -> (
+        with Not_found -> error("Class \"" ^ class_name ^ "\" does not exist. Did you mean \"" ^ suggest_class class_name cenv ^ "\" ?")
+      )
+    | NewCstr (class_name, args) -> (
         try(
-          let class_definition = Env.find cenv class_name in
+          let class_definition = Env.find class_name cenv in
           try (
-            let method = List.find (fun method_definition -> 
+            let constructor = List.find (fun method_definition -> 
               method_definition.method_name = "constructor") class_definition.methods in
             
             try (
-              if method.return <> TVoid then
+              if constructor.return <> TVoid then
                 error("Constructor of class \"" ^ class_name ^ "\" must return void");
               
               List.iter2 (fun arg (_, arg_exp_type) -> 
                 check arg arg_exp_type tenv cenv
-              ) args method.params;
+              ) args constructor.params;
               
                 
               TClass class_name 
             ) with Invalid_argument _ -> 
               error("Constructor of class \"" ^ class_name ^ "\" expects " ^ 
-                    string_of_int (List.length method.params) ^ " arguments but got " ^ 
+                    string_of_int (List.length constructor.params) ^ " arguments but got " ^ 
                     string_of_int (List.length args))
 
           ) with Not_found -> 
             error("No constructor method defined for class \"" ^ class_definition.class_name ^ "\".")
         ) with Not_found -> 
-          error("Class \"" ^ class_name ^ "\" does not exist.")
+            error("Class \"" ^ class_name ^ "\" does not exist. Did you mean \"" ^ suggest_class class_name cenv ^ "\" ?") 
     )
-    | MethCall(obj_expr, method_name, args) -> (
+    | This -> (
+        try Env.find "this" tenv
+        with Not_found -> error "\"this\" used outside of class context"
+    )
+    | MethCall (obj_expr, method_name, args) -> (
         let rec find_method class_name =
             let cls = try 
-                Env.find cenv class_name 
+                Env.find class_name cenv 
             with Not_found -> 
                 error("Undefined class: " ^ class_name) in
             
@@ -130,9 +227,17 @@ let typecheck_prog p =
                          " arguments but got " ^ string_of_int (List.length args))
                          
             with Not_found -> 
-                match cls.parent with
-                | None -> error("Method '" ^ method_name ^ "' not found in class " ^ class_name)
-                | Some parent_name -> find_method parent_name
+              match cls.parent with
+              | None -> 
+                  (*get back the original class name because "class_name" right now is the oldest ancestor*)
+                  (match type_expr obj_expr tenv cenv with 
+                  | TClass cls_name ->
+                      error("Method '" ^ method_name ^ "' not found in class \"" ^ cls_name ^ 
+                            "\". Did you mean \"" ^ suggest_meth method_name cls_name cenv ^ "\"?")
+                  | _ -> failwith "unreachable"
+                  )
+              | Some parent_name -> find_method parent_name
+            
         in
         
         match type_expr obj_expr tenv cenv with
@@ -142,21 +247,22 @@ let typecheck_prog p =
     | _ -> failwith "case not implemented in type_expr (should be unreachable)"
 
   and type_mem_access m tenv cenv = match m with
-    | Var x -> (try Env.find tenv x
-                with Not_found -> error("Undefined variable \"" ^ x ^ "\"." ))
+    | Var x -> (try Env.find x tenv
+                with Not_found -> error("Undefined variable \"" ^ x ^ "\". Did you mean \"" ^ suggest_attr_or_var x None tenv cenv ^ "\"?"))
     | Field(obj_expr, field_name) ->(
         let rec find_field class_name =  
             let cls = try 
-                Env.find cenv class_name 
+                Env.find class_name cenv
             with Not_found -> 
                 error("Undefined class: " ^ class_name) in
             
             try 
                 snd (List.find (fun elem -> fst elem = field_name) cls.attributes)
             with Not_found -> 
-                match cls.parent with
-                | None -> error("Field '" ^ field_name ^ "' not found in class " ^ class_name)
-                | Some parent_name -> find_field parent_name
+              match cls.parent with
+              | None -> error("Field '" ^ field_name ^ "' not found in class \"" ^ class_name ^ 
+                              "\". Did you mean \"" ^ suggest_attr_or_var field_name (Some class_name) tenv cenv ^ "\"?")
+              | Some parent_name -> find_field parent_name
         in        
         match type_expr obj_expr tenv cenv with
         | TClass class_name -> find_field class_name
@@ -206,23 +312,30 @@ let rec check_method class_name meth tenv cenv =
   check_seq meth.code meth.return tenv cenv
 
 and check_class cls cenv =
-  ( (*check parent existance*)
-  match cls.parent with
+  (* Check parent existence *)
+  (match cls.parent with
     | Some parent_name ->
         if not (Env.mem parent_name cenv) then
-          error ("Unknown parent class \"" ^ parent_name ^ "\" for class \"" ^ cls.class_name ^ "\"")
+          error ("Unknown parent class \"" ^ parent_name ^ "\" for class \"" ^ cls.class_name ^ 
+                 "\". Did you mean \"" ^ suggest_class parent_name cenv ^ "\"?")
     | None -> ()
   );
 
   let tenv = add_env cls.attributes Env.empty in
 
+  (* Check attribute types *)
   List.iter (fun (_, typ) ->
     match typ with
-    | TClass class_name -> 
-        try Env.find class_name cenv with Not_found -> error("Unknown class \"" ^ class_name ^ "\".")
+    | TClass class_name -> (
+        if not (Env.mem class_name cenv) then
+          error("Unknown class \"" ^ class_name ^
+                "\". Did you mean \"" ^ suggest_class class_name cenv ^ "\"?")
+    )
     | _ -> ()
-  ) cls.attributes
+    
+  ) cls.attributes;
   
+  (* Check methods *)
   List.iter (fun meth -> check_method cls.class_name meth tenv cenv) cls.methods
 in
 
